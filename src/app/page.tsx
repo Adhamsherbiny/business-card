a471a7af06d91848a5e388ddef88b29e6af03c0c
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2024-09-15 12:57:05 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import "./homepage.css";
+import { useRef } from "react";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const logInSection = useRef<any>();
+  const singUpSection = useRef<any>();
+  const animationTemplet = useRef<any>();
+  const startAnimation = useRef<any>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  //! LOADING
+  function loading(time: number) {
+    if (animationTemplet.current.style.display == "none") {
+      animationTemplet.current.style.display = "flex";
+      startAnimation.current.style.animationName = "identifier";
+    }
+    setTimeout(() => {
+      animationTemplet.current.style.display = "none";
+      startAnimation.current.style.animationName = "";
+    }, time);
+  }
+  //! END LOADING
+  function showlogin() {
+    logInSection.current.style.display = "flex";
+    singUpSection.current.style.display = "none";
+    loading(3000);
+  }
+  function showsingup() {
+    logInSection.current.style.display = "none";
+    singUpSection.current.style.display = "flex";
+    loading(3000);
+  }
+  return (
+    <div className="page">
+      <div className="login" ref={logInSection}>
+        <div className="txt">
+          <h1>Electronic Business Card</h1>
+          <p>Login to enjoy our services</p>
+          <p>
+            Do not have account <span onClick={showsingup}>Register</span>
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <form action="" autoComplete="off">
+          <div className="form-div">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="form-div">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <input className="login-inp" type="submit" value="Login" />
+        </form>
+      </div>
+      <div className="singup" ref={singUpSection}>
+        <div className="txt">
+          <h1>Electronic Business Card</h1>
+          <p>Sing up to enjoy our services</p>
+          <p>
+            Already have account <span onClick={showlogin}>Login</span>
+          </p>
+        </div>
+        <form action="" autoComplete="off">
+          <div className="form-div">
+            <label htmlFor="singup-username">Username</label>
+            <input type="text" id="singup-username" />
+          </div>
+          <div className="form-div">
+            <label htmlFor="singup-password">Password</label>
+            <input type="password" id="singup-password" />
+          </div>
+          <div className="form-div">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <input className="singup-inp" type="submit" value="Register" />
+        </form>
+      </div>
+      {/* Loading */}
+      <div className="loading" ref={animationTemplet}>
+        <div className="txt" ref={startAnimation}>
+          Loading
+        </div>
+      </div>
+      {/* End Loading */}
     </div>
   );
 }
